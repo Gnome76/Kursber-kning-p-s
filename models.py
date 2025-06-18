7ca2c5329b2854db2d14a76bf710966edd729ada
Gnome76 <magber542@gmail.com>
2025-06-18 16:07:56 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,15 @@
+from dataclasses import dataclass
 
+@dataclass
+class Bolag:
+    id: int
+    bolagsnamn: str
+    nuvarande_kurs: float
+    omsattning_i_ar: float
+    omsattning_nasta_ar: float
+    antal_aktier: int
+    ps1: float
+    ps2: float
+    ps3: float
+    ps4: float
+    ps5: float
